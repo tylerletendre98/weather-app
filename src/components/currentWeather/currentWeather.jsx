6f8c758ecd2dc13ff538240eb5ed198e65d1621f
T U <tylerletendre98@gmail.com>
2021-08-19 17:48:59 -0400

    added in some styling made the page look cleaner

--- src/components/currentWeather/currentWeather.jsx
+++ src/components/currentWeather/currentWeather.jsx
@@ -1,9 +1,34 @@
 const CurrentWeather = (props) => {
-  console.log(props);
   return (
     <div>
-      <h3>{props.currentWeather.location.name}</h3>
-      <p>temperature: {props.currentWeather.current.temp_f}</p>
+      <div className="container">
+        <div className="row">
+          <div class="col-2"></div>
+          <div class="col-8">
+            <div className="title-header">
+              <div className="weather-containter">
+                <div className="location">
+                  <h3>
+                    {props.currentWeather.location.name},
+                    {props.currentWeather.location.region}
+                    <img
+                      src="//cdn.weatherapi.com/weather/64x64/day/116.png"
+                      alt="condition image"
+                    />
+                  </h3>
+                </div>
+                <div className="location-conditions">
+                  <p>temperature: {props.currentWeather.current.temp_f}</p>
+                  <p>feels like: {props.currentWeather.current.feelslike_f}</p>
+                  <p>Winds: {props.currentWeather.current.wind_mph}</p>
+                  <p>Wind direction: {props.currentWeather.current.wind_dir}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-2"></div>
+        </div>
+      </div>
     </div>
   );
 };
