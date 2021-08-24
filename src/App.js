0adff19a40baf8cd33e56683d653abddac97039a
T U <tylerletendre98@gmail.com>
2021-08-24 09:29:41 -0400

    added in static data to save on API calls

--- src/App.js
+++ src/App.js
@@ -4,39 +4,42 @@ import axios from "axios";
 import Title from "./components/title/title";
 import APIKEY from "./key";
 import data from "./data";
+import forecastWeatherData from "./forecastWeatherData";
 import CurrentWeather from "./components/currentWeather/currentWeather";
 import Searchbar from "./components/searchBar/searchBar";
+import ForecastWeather from "./components/forcastWeather/forecastWeather";
 
 class App extends Component {
   state = {
     cityName: "",
   };
-  // componentDidMount() {
-  //   this.setState({ weather: data });
-  // }
   componentDidMount() {
-    axios
-      .get(
-        `http://api.weatherapi.com/v1/current.json?key=${APIKEY}&q=pawtucket&aqi=no`
-      )
-      .then((res) => {
-        const weather = res.data;
-        this.setState({ weather: weather });
-      });
+    this.setState({ weather: data });
+    this.setState({ forecastWeatherData: forecastWeatherData });
   }
+  // componentDidMount() {
+  //   axios
+  //     .get(
+  //       `http://api.weatherapi.com/v1/current.json?key=${APIKEY}&q=pawtucket&aqi=no`
+  //     )
+  //     .then((res) => {
+  //       const weather = res.data;
+  //       this.setState({ weather: weather });
+  //     });
+  // }
 
-  componentDidUpdate() {
-    if (this.state.cityName != "") {
-      axios
-        .get(
-          `http://api.weatherapi.com/v1/current.json?key=${APIKEY}&q=${this.state.cityName}&aqi=no`
-        )
-        .then((res) => {
-          const weather = res.data;
-          this.setState({ weather: weather });
-        });
-    }
-  }
+  // componentDidUpdate() {
+  //   if (this.state.cityName != "") {
+  //     axios
+  //       .get(
+  //         `http://api.weatherapi.com/v1/current.json?key=${APIKEY}&q=${this.state.cityName}&aqi=no`
+  //       )
+  //       .then((res) => {
+  //         const weather = res.data;
+  //         this.setState({ weather: weather });
+  //       });
+  //   }
+  // }
 
   setCityName = (searchBarResults) => {
     console.log(searchBarResults);
@@ -57,6 +60,7 @@ class App extends Component {
           <Title />
           <Searchbar setCityName={this.setCityName} />
           <CurrentWeather currentWeather={this.state.weather} />
+          <ForecastWeather />
         </div>
       );
     }
---
+++ src/components/forcastWeather/forecastWeather.jsx
@@ -0,0 +1,11 @@
+import "./forecastWeather.css";
+
+const ForecastWeather = () => {
+  return (
+    <div>
+      <h1>forecast weather</h1>
+    </div>
+  );
+};
+
+export default ForecastWeather;
