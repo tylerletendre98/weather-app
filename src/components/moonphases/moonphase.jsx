03fefeab760e922c1b381e4c6cad27c82b5ce588
T U <tylerletendre98@gmail.com>
2021-09-09 16:24:32 -0400

    added styling

--- src/components/moonphases/moonphase.jsx
+++ src/components/moonphases/moonphase.jsx
@@ -1,3 +1,5 @@
+import './moonphase.css'
+
 const MoonPhase = (props)=>{
     return(
         <div>
@@ -7,17 +9,17 @@ const MoonPhase = (props)=>{
 
                         </div>
                         <div className="col-9">
-                        <div className="mooninfo-container">
-                        <div>
-                            <h3>Moon Infomation</h3>
-                        </div>
-                        <div>
-                            <p>Moonrise: {props.moonInfo.moonrise}</p>
-                            <p>Moonset: {props.moonInfo.moonset}</p>
-                            <p>Moon phase: {props.moonInfo.moon_phase}</p>
-                            <p>Moon Illumination: {props.moonInfo.moon_illumination}</p>
-                        </div>
-                        </div>
+                            <div className="mooninfo-container">
+                                <div>
+                                    <h3>Moon Infomation</h3>
+                                </div>
+                                <div>
+                                    <p>Moonrise: {props.moonInfo.moonrise}</p>
+                                    <p>Moonset: {props.moonInfo.moonset}</p>
+                                    <p>Moon phase: {props.moonInfo.moon_phase}</p>
+                                    <p>Moon Illumination: {props.moonInfo.moon_illumination}</p>
+                                </div>
+                            </div>
                         </div>
                         <div className="col-2">
                         </div>
